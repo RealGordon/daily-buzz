e99165e3b213e368d8c036a3856e520996f69180
RealGordon <92362350+RealGordon@users.noreply.github.com>
2021-11-10 08:56:07 +0000

    daily buzz

---
+++ README.md
@@ -0,0 +1,11 @@
+## SETTING UP YOUR DEV ENVIRONMENT
+# Daily Buzz
+
+**author** : **gordon.amamoo@amalitech.org**
+
+
+In this project, I’ll use the commands I just learned to navigate through the files and directories of Daily Buzz, a national newspaper.
+
+### Links:
+
+https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-setting-up-your-dev-environment/modules/fecp-command-line-for-building-websites/projects/daily-buzz
---
+++ daily_buzz.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#gordon.amamoo@amalitech.org
+#1.Print the working directory.
+pwd
+
+#2.List all files and directories in the current working directory
+ls
+
+#3.With one command, change directories to the national/politics/ directory.
+cd national/politics
+
+#4.List all files and directories in the working politics/ directory.
+ls
+
+#5.In the politics/ directory, create a directory called elections/
+mkdir elections
+
+#6.Change directories into the elections/ directory.
+cd elections
+
+#7.In the elections/ directory, make two files candidates.txt and issues.txt
+touch candidates.txt && touch issues.txt
+
+#8.Change directories three levels up to the daily-buzz/ directory, and print the working directory.
+cd ../../.. && pwd
+
+#9.In the daily-buzz/ directory, make a directory called business/ and change directories into the business/ directory.
+mkdir business && cd business
+
+#10.List all files and directories in the business/ directory.
+ls
+
+#11.From the business/ directory, make a directory called startups/ and change directories into the startups/ directory.
+mkdir startups && cd startups
+
+#12.Change directories one level up back to the business/ directory.
+#From the business/ directory, make a directory that is a child of startups/, called disruptors/.
+cd .. && mkdir startups/disruptors
+
+#13.From the business/ directory, make three files in the disruptors/ directory. The files should be called tech.txt design.txt and education.txt.
+touch  startups/disruptors/tech.txt && touch  startups/disruptors/design.txt && touch  startups/disruptors/education.txt
+
+#14.Change directories one level up to the daily-buzz/ directory and print the working directory.
+cd ..
